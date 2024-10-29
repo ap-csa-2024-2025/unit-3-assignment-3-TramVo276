14aaf28c49ec2dc8b08044585529b00b029fb5ce
TramVo276 <tvo941352@gmail.com>
2024-10-29 10:04:26 -0700

    dg

--- Main.java
+++ Main.java
@@ -4,12 +4,38 @@ public class Main
 {
   public static void main(String[] args)
   {
-    // TODO: Problem 1
+    Scanner sc = new Scanner (System.in)// TODO: Problem 1
+    System.out.println("Error int number");
+    int x = sc.nextln();
+    int y = sc.nextln();
+    if (y==0)
+    {
+      System.out.println("Error");
+    else 
+    {
+      double ratio = (double)(x/y);
+      if (1 <= ratio && ratio <= 8)
+      {
+        System.out.println("Ratio OK");
+      }
+    }
+    }
+    
 
-
-    // TODO: Problem 2
-
-
+    Scanner sc = new Scanner (System.in)// TODO: Problem 2
+    int a = sc.nextln();
+    int b = sc.nextln();
+    if (b==0)
+    {
+      System.out.println("Error");
+    else 
+    {
+      int factor = a % b;
+      if factor = 0
+      {
+        System.out.print("Is a factor");
+      }
+    }
     // TODO: Problem 3
   }
 
@@ -18,9 +44,9 @@ public class Main
   // true if they are equal, and false otherwise.  Two
   // Rectangles are considered equal if their widths are equal,
   // and if their lengths are equal.
-  public static boolean rectEqual(Rectangle r1, Rectangle r2)
-  {
+  //public static boolean rectEqual(Rectangle r1, Rectangle r2)
+  //{
     // implement solution here
-    return false;
-  }
+    //return false;
+ }
 }
